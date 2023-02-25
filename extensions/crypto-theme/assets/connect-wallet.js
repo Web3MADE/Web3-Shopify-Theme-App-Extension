01227328476bab10b0a60d3dd6c0c9a8603b94aa
Hakeem <mwo248@yahoo.com>
2023-02-25 17:43:51 +0000

    connect-wallet JS + CSS

---
+++ extensions/crypto-theme/assets/connect-wallet.js
@@ -0,0 +1,57 @@
+import { Web3Provider } from "@ethersproject/providers";
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+
+let provider;
+const CONNECTED = "Connected";
+const button = document.getElementById("connect-wallet");
+button.addEventListener("click", connectWallet);
+async function getWeb3Modal() {
+  const web3Modal = new Web3Modal({
+    network: "mainnet",
+    cacheProvider: true,
+    providerOptions: {
+      walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+          // for live prod: process.env.INFURA_ID
+          infuraId: process.env.INFURA_ID,
+        },
+      },
+      coinbasewallet: {
+        package: CoinbaseWalletSDK,
+        options: {
+          appName: "APP NAME",
+          infuraId: process.env.INFURA_ID,
+        },
+      },
+    },
+  });
+  return web3Modal;
+}
+
+async function connectWallet() {
+  const web3Modal = await getWeb3Modal();
+  const connection = await web3Modal.connect();
+  provider = new Web3Provider(connection);
+
+  const accounts = await provider.listAccounts();
+
+  if (accounts[0]) {
+    button.textContent = CONNECTED;
+    localStorage.setItem("fullAddr", accounts[0]);
+  }
+
+  if (accounts.length === 0) return;
+}
+
+Window.addEventListener("DOMContentLoaded", async (event) => {
+  event.preventDefault();
+
+  const stored = localStorage.getItem("fullAddr");
+
+  if (window.ethereum && stored) {
+    button.textContent = CONNECTED;
+  }
+});
